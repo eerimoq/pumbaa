b28a812794f50e8a27354cbb841274941abd4523
Erik Moqvist <erik.moqvist@gmail.com>
2016-09-02 23:21:40 +0200

    Moved prot files to source folder.

--- src/mpconfigport.h
+++ src/mpconfigport.h
@@ -1,27 +1,20 @@
-/*
- * This file is part of the Micro Python project, http://micropython.org/
+/**
+ * @file libpumbaa.c
  *
- * The MIT License (MIT)
+ * @section License
+ * Copyright (C) 2014-2016, Erik Moqvist
  *
- * Copyright (c) 2015 Damien P. George
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This file is part of the Pumbaa project.
  */
 
 // options to control how Micro Python is built
@@ -132,6 +125,8 @@ typedef long long mp_off_t;
 #else
 typedef long mp_off_t;
 #endif
+
+#define mp_hal_pin_obj_t mp_obj_t
 
 // We need to provide a declaration/definition of alloca()
 #include <alloca.h>
